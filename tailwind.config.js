ee1c89a24edad158b0a46d8518a85ffd2126c044
Abdullahi Hussein <miraacle64@gmail.com>
2022-01-31 15:08:39 +0100

    feat:footer done with responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,8 @@ module.exports = {
         footerText: "hsl(240, 2%, 79%)",
         whiteText:"hsl(0, 0%, 100%)",
         footerBg: "hsl(240, 10%, 16%)" ,
+        sectionBg1: "hsl(237, 17%, 21%)",
+        sectionBg2: "hsl(237, 23%, 32%)",
         ctaText:"hsl(356, 100%, 66%)",
         ctaHoverBg:"hsl(355, 100%, 74%)",
         headings: "hsl(208, 49%, 24%)"
