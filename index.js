2d8a722a9a832b5c7641b74af850f7f711f05812
Abdullahi Hussein <miraacle64@gmail.com>
2022-02-02 09:20:51 +0100

    feat: project fully done though still a little bit buggy

--- index.js
+++ index.js
@@ -2,9 +2,13 @@
 
 const menuOpenBtn = document.getElementById("menu-open__btn");
 const menuCloseBtn = document.getElementById("menu-close__btn");
-const menuContent = document.getElementById("mobile-menu__content");
+const menuContent = document.querySelector("#mobile-menu__content");
+const signupBtn = document.getElementById("sign-up");
+// console.log(menuContent);
 
-console.log(menuContent)
+
+
+
 const productsLinksBtn = document.querySelector("#product__links--btn");
 const  productsLinksContainer = document.querySelector("#product__links");
 const  productsLinksIcon = document.querySelector("#product__links--icon");
@@ -20,6 +24,30 @@ const  companyLinksIcon = document.querySelector("#company__links--icon");
 const connectLinksBtn = document.querySelector("#connect__links--btn");
 const  connectLinksContainer = document.querySelector("#connect__links");
 const  connectLinksIcon = document.querySelector("#connect__links--icon");
+
+
+
+//MOBILE NAV
+
+let click = 0; 
+
+const   mobProductsLinksBtn = document.querySelector("#mob-product__links--btn");
+const   mobProductsLinksBtnContainer = document.querySelector("#mob-product__links--btn-container");
+const  mobProductsLinksContainer = document.querySelector("#mob-product__links");
+const  mobProductsLinksIcon = document.querySelector("#mob-product__links--icon");
+
+
+const mobCompanyLinksBtn = document.querySelector("#mob-company__links--btn");
+const mobCompanyLinksBtnContainer = document.querySelector("#mob-company__links--btn-container");
+const  mobCompanyLinksContainer = document.querySelector("#mob-company__links");
+const  mobCompanyLinksIcon = document.querySelector("#mob-company__links--icon");
+
+
+
+const mobConnectLinksBtn = document.querySelector("#mob-connect__links--btn");
+const mobConnectLinksBtnContainer = document.querySelector("#mob-connect__links--btn");
+const  mobConnectLinksContainer = document.querySelector("#mob-connect__links");
+const  mobConnectLinksIcon = document.querySelector("#mob-connect__links--icon");
 // console.log(headerLinkBtns)
 // console.log(headerLinkDropdowns)
 
@@ -60,11 +88,12 @@ const handleConnectDropdownClose = function(){
     connectLinksIcon.style.transform = "rotate(0deg)";
 }
 
+//MOBILE ACTIONS
 
 
 
 
-const handleHeaderEvents = function() {
+const handleDesktopHeaderEvents = function() {
 
     productsLinksBtn.addEventListener("mouseenter",handleProductDropdownOpen);
     productsLinksContainer.addEventListener("mouseleave",handleProductDropdownClose);
@@ -82,20 +111,98 @@ const handleHeaderEvents = function() {
     connectLinksContainer.addEventListener("mouseenter",handleConnectDropdownOpen);
 
 
+    
+}
+
+const mobileProductDropdown = function (){
+    mobProductsLinksIcon.style.transition = "all .3s linear";
+    
+    if(click <= 1){
+        click++;
+    }
+    
+
+    if(click === 1){
+        mobProductsLinksContainer.classList.remove("hidden");
+        mobProductsLinksIcon.style.transform = "rotate(180deg)";
+    }
+
+    if(click === 2){
+        click = 0;
+        mobProductsLinksContainer.classList.add("hidden");
+        mobProductsLinksIcon.style.transform = "rotate(0deg)";
+    }
+    
+}
+const mobileCompanyDropdown = function (){
+    mobCompanyLinksIcon.style.transition = "all .3s linear";
+    if(click <= 1){
+        click++;
+    }
+
+    if(click === 1){
+        mobCompanyLinksContainer.classList.remove("hidden");
+        mobCompanyLinksIcon.style.transform = "rotate(180deg)";
+    }
+
+    if(click === 2){
+        click = 0;
+        mobCompanyLinksContainer.classList.add("hidden");
+        mobCompanyLinksIcon.style.transform = "rotate(0deg)";
+    }
+    
+}
+
+const mobileConnectDropdown = function (){
+    mobConnectLinksIcon.style.transition = "all .3s linear";
+    if(click <= 1){
+        click++;
+    }
+
+    if(click === 1){
+        mobConnectLinksContainer.classList.remove("hidden");
+        mobConnectLinksIcon.style.transform = "rotate(180deg)";
+    }
+
+    if(click === 2){
+        click = 0;
+        mobConnectLinksContainer.classList.add("hidden");
+        mobConnectLinksIcon.style.transform = "rotate(0deg)";
+    }
+    
+}
+
+
+const handleMobileHeaderEvents = function() {
+    const handleCloseNav = function(){
+        menuCloseBtn.style.display = "none";
+        menuOpenBtn.style.display = "block";
+        menuContent.style.top = "-200%";
+    }
+
     menuOpenBtn.addEventListener("click",() => {
         menuCloseBtn.style.display = "block";
         menuOpenBtn.style.display = "none";
-        // menuContent.style.visibility = "visible";
-        menuContent.style.opacity = "1";
-    })
-
-    menuCloseBtn.addEventListener("click",() => {
-        menuCloseBtn.style.display = "none";
-        menuOpenBtn.style.display = "block";
-        // menuContent.style.visibility = "hidden";
-        menuContent.style.opacity = "0";
         
+        menuContent.style.top = "75px";
     })
+    signupBtn.addEventListener("click",handleCloseNav)
+
+    menuCloseBtn.addEventListener("click",handleCloseNav)
+
+    mobProductsLinksBtn.addEventListener("click",mobileProductDropdown);
+
+    mobCompanyLinksBtn.addEventListener("click",mobileCompanyDropdown);
+
+    mobConnectLinksBtn.addEventListener("click",mobileConnectDropdown);
+    
+}
+
+
+
+const handleHeaderEvents = function () {
+    handleDesktopHeaderEvents();
+    handleMobileHeaderEvents();
 }
 
 handleHeaderEvents();
